ac17609c20765948fbb4b91469ec3b4dc4495ffd
helly25 <6420169+helly25@users.noreply.github.com>
2026-09-13 23:12:56 +0100

    Make HAMT insertion construction pointer const

--- mbo/container/internal/hamt_shared_node.h
+++ mbo/container/internal/hamt_shared_node.h
@@ -111,13 +111,13 @@ class HamtSharedNode final {
     if (!result) {
       return std::nullopt;
     }
-    node_type* const node = *result;
+    const node_type* const node = *result;
     const auto entries = original.entries();
     std::uninitialized_copy_n(entries.begin(), position, node->EntryPtr());
     std::construct_at(node->EntryPtr() + position, entry);
     std::uninitialized_copy(
         entries.begin() + static_cast<std::ptrdiff_t>(position), entries.end(), node->EntryPtr() + position + 1);
-    return node;
+    return result;
   }
 
   template<mbo::memory::BlockSource Source>
